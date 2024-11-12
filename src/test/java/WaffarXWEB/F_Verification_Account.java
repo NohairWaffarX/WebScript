14d60f0359b2e34b0c97ff1125221fee485eaba8
NohairWaffarX <nohair.gamal@waffarx.com>
2024-11-12 13:49:00 +0200

    Update F_Verification_Account.java

--- src/test/java/WaffarXWEB/F_Verification_Account.java
+++ src/test/java/WaffarXWEB/F_Verification_Account.java
@@ -40,39 +40,44 @@ public class F_Verification_Account
         SendCode_Button = By.id("SendCode");
     }
 
-    // @Test
-    // public void AA_Check_that_ErrorAppear_whenInsert_Number_AlreadyUsed()
-    // {
-    //     Open_PopupOF_VerifyAccount() ;
-    //     driver.element().type(PhoneNumber_Field, "01277249225");
-    //     driver.element().click(SendCode_Button);
-    //     By Error= By.className("ExistedMobNumber") ;
-    //     driver.element().verifyThat(Error).text().isEqualTo("This mobile number has already been registered.").perform();
-    // }
+    @Test
+    public void AA_Check_that_ErrorAppear_whenInsert_Number_AlreadyUsed()
+    {
+        PhoneNumber_Field = By.id("phone");
+        Open_PopupOF_VerifyAccount() ;
+        driver.element().type(PhoneNumber_Field, "01277249225");
+        driver.element().click(SendCode_Button);
+        By Error= By.className("ExistedMobNumber") ;
+        driver.element().verifyThat(Error).text().isEqualTo("This mobile number has already been registered.").perform();
+    }
 
-    // @Test
-    // public void B_Check_that_ErrorAppear_whenInsert_LessThan_MinLimitation_InMobileNumber()
-    // {
-    //     Open_PopupOF_VerifyAccount() ;
-    //     driver.element().type(PhoneNumber_Field, "012");
-    //     driver.element().click(SendCode_Button);
-    //     By Error= By.id("phone-error");
-    //     driver.element().verifyThat(Error).text().isEqualTo("Sorry, Arabic and special characters are not allowed, please make sure you enter a valid input.").perform();
-    // }
+    @Test
+    public void B_Check_that_ErrorAppear_whenInsert_LessThan_MinLimitation_InMobileNumber()
+    {
+        PhoneNumber_Field = By.id("phone");
+        Open_PopupOF_VerifyAccount() ;
+        driver.element().type(PhoneNumber_Field, "012");
+        driver.element().click(SendCode_Button);
+        By Error= By.id("phone-error");
+        driver.element().verifyThat(Error).text().isEqualTo("Sorry, Arabic and special characters are not allowed, please make sure you enter a valid input.").perform();
+    }
 
-    // @Test
-    // public void C_Check_that_ErrorAppear_whenInsert_GreaterThan_MaxLimitation_InMobileNumber()
-    // {
-    //     Open_PopupOF_VerifyAccount() ;
-    //     driver.element().type(PhoneNumber_Field, "0127724999999999888");
-    //     driver.element().click(SendCode_Button);
-    //     By Error= By.id("WrongEgMobNumber");
-    //     driver.element().verifyThat(Error).text().isEqualTo("Please make sure that phone number is true, contains only numbers and consist of 11 number.").perform();
-    // }
+    @Test
+    public void C_Check_that_ErrorAppear_whenInsert_GreaterThan_MaxLimitation_InMobileNumber()
+    {
+        PhoneNumber_Field = By.id("phone");
+        Open_PopupOF_VerifyAccount() ;
+        driver.element().type(PhoneNumber_Field, "0127724999999999888");
+        driver.element().click(SendCode_Button);
+        By Error= By.id("WrongEgMobNumber");
+        driver.element().verifyThat(Error).text().isEqualTo("Please make sure that phone number is true, contains only numbers and consist of 11 number.").perform();
+
+    }
     @Test
     public void D_Verify_Account_exceed3times() throws InterruptedException {
         Open_PopupOF_VerifyAccount() ;
         Thread.sleep(5000);
+        PhoneNumber_Field = By.id("phone");
         driver.element().type(PhoneNumber_Field, "01067802082");
         driver.element().click(SendCode_Button);  // After click on it , First otp send
 
@@ -84,25 +89,25 @@ public class F_Verification_Account
         driver.element().click(ReSend) ; // After click on it , third otp send
 
         Thread.sleep(140000);
-        driver.element().click(ReSend) ;
+        driver.element().click(ReSend) ;// After click on it , Error Message appear
 
         By Error= By.id("ExceedCount") ;
         driver.element().verifyThat(Error).text().isEqualTo("You exceed the number of times the activation code has been sent, please contact customer service to complete the activation process").perform();
     }
 
-    // @Test(dependsOnMethods = { "D_Verify_Account_exceed3times" })
-    // public void E_Check_that_ErrorAppear_whenInsert_WrongVerificationCode() {
-    //     Open_PopupOF_VerifyAccount();
+    @Test(dependsOnMethods = { "D_Verify_Account_exceed3times" })
+    public void E_Check_that_ErrorAppear_whenInsert_WrongVerificationCode() {
+        Open_PopupOF_VerifyAccount();
 
-    //     By VerificationCode_Text = By.xpath("(//*[@id='VerificationCode'])[2]");
-    //     driver.element().type(VerificationCode_Text, "88888");
+        By VerificationCode_Text = By.xpath("(//*[@id='VerificationCode'])[2]");
+        driver.element().type(VerificationCode_Text, "88888");
 
-    //     By VerifyAccount_Button = By.xpath("//*[@id=\"VerifyUserPhoneNumber\"]/input");
-    //     driver.element().click(VerifyAccount_Button);
+        By VerifyAccount_Button = By.xpath("//*[@id=\"VerifyUserPhoneNumber\"]/input");
+        driver.element().click(VerifyAccount_Button);
 
-    //     By Error = By.xpath("//*[@id=\"WrongVerify\"]");
-    //     driver.element().verifyThat(Error).text().isEqualTo("Invalid verification Code!").perform();
-    // }
+        By Error = By.xpath("//*[@id=\"WrongVerify\"]");
+        driver.element().verifyThat(Error).text().isEqualTo("Invalid verification Code!").perform();
+    }
     @AfterMethod
     public void CloseDriver()
     {
