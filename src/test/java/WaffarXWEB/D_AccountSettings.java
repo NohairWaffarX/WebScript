3ae107eb2883787f849438f982089f4b6decfc76
NohairWaffarX <nohair.gamal@waffarx.com>
2024-11-16 15:31:35 +0200

    Update D_AccountSettings.java

--- src/test/java/WaffarXWEB/D_AccountSettings.java
+++ src/test/java/WaffarXWEB/D_AccountSettings.java
@@ -15,15 +15,11 @@ public class D_AccountSettings
         driver = new SHAFT.GUI.WebDriver(); // to open browser
         driver.browser().navigateToURL("https://www.waffarx.com/en-eg");
     }
-    private void ensureElementReady(By locator) {
-        driver.element().waitToBeReady(locator);
-    }
 
     private void retryClick(By locator, int maxRetries) {
         int attempt = 0;
         while (attempt < maxRetries) {
             try {
-                ensureElementReady(locator);
                 driver.element().click(locator);
                 System.out.println("Successfully clicked the element on attempt " + (attempt + 1));
                 return; // Exit the method if click is successful
@@ -45,7 +41,6 @@ public class D_AccountSettings
         int attempt = 0;
         while (attempt < maxRetries) {
             try {
-                ensureElementReady(locator);
                 driver.element().type(locator, text);
                 System.out.println("Successfully typed into the element on attempt " + (attempt + 1));
                 return; // Exit the method if typing is successful
@@ -67,7 +62,6 @@ public class D_AccountSettings
         int attempt = 0;
         while (attempt < maxRetries) {
             try {
-                ensureElementReady(locator);
                 driver.element().hover(locator);
                 System.out.println("Successfully clicked the element on attempt " + (attempt + 1));
                 return; // Exit the method if click is successful
