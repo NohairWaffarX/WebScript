a074bee32515cf46bbc4e35d911cfa0d03a672a6
NohairWaffarX <nohair.gamal@waffarx.com>
2024-11-16 15:30:03 +0200

    Update A_SignUP_Page.java

--- src/test/java/WaffarXWEB/A_SignUP_Page.java
+++ src/test/java/WaffarXWEB/A_SignUP_Page.java
@@ -15,15 +15,10 @@ public class A_SignUP_Page
         driver = new SHAFT.GUI.WebDriver();
         driver.browser().navigateToURL("https://portal-test.waffarx.com/en-eg");
     }
-    private void ensureElementReady(By locator) {
-        driver.element().waitToBeReady(locator);
-    }
-
     private void retryType(By locator, String text, int maxRetries) {
         int attempt = 0;
         while (attempt < maxRetries) {
             try {
-                ensureElementReady(locator);
                 driver.element().type(locator, text);
                 System.out.println("Successfully typed into the element on attempt " + (attempt + 1));
                 return; // Exit the method if typing is successful
