3a87fc361799ab62741de70c34afb42454d3e395
NohairWaffarX <nohair.gamal@waffarx.com>
2024-11-16 17:42:08 +0200

    Update A_SignUP_Page.java

--- src/test/java/WaffarXWEB/A_SignUP_Page.java
+++ src/test/java/WaffarXWEB/A_SignUP_Page.java
@@ -58,7 +58,7 @@ public class A_SignUP_Page
     public void A_Check_that_ErrorAppear_whenNameIs_SymbolsANDChars()
     {
         clickRegisterButton();
-        retryType(FullName, "@#$456", 30);
+        retryType(FullName, "@#$456", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(NameError).text().isEqualTo("Full name must be characters only").perform();
     }
@@ -67,7 +67,7 @@ public class A_SignUP_Page
     public void B_Check_that_ErrorAppear_whenName_isEmpty()
     {
         clickRegisterButton() ;
-        retryType(FullName, " ", 30);
+        retryType(FullName, " ", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(NameError).text().isEqualTo("Full name minimum length is 3 character").perform();
     }
@@ -76,7 +76,7 @@ public class A_SignUP_Page
     public void C_Check_that_Name_has_max_limitation_as_50char()
     {
         clickRegisterButton() ;
-        retryType(FullName, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 30);
+        retryType(FullName, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(NameError).text().isEqualTo("Full name maximum length is 50 character").perform();
     }
@@ -85,7 +85,7 @@ public class A_SignUP_Page
     public void D_Check_that_Name_has_min_limitation_as_3char()
     {
         clickRegisterButton() ;
-        retryType(FullName, "AA", 30);
+        retryType(FullName, "AA", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(NameError).text().isEqualTo("Full name minimum length is 3 character").perform();
     }
@@ -94,7 +94,7 @@ public class A_SignUP_Page
     public void E_Check_that_ErrorAppear_whenEmail_isEmpty()
     {
         clickRegisterButton() ;
-        retryType(Email, " ", 30);
+        retryType(Email, " ", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(EmailError).text().isEqualTo("The e-mail that you entered is wrong").perform();
     }
@@ -103,7 +103,7 @@ public class A_SignUP_Page
     public void F_Check_that_ErrorAppear_whenInsert_WrongFormat_inEmail()
     {
         clickRegisterButton() ;
-        retryType(Email, "nohair", 30);
+        retryType(Email, "nohair", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(EmailError).text().isEqualTo("The e-mail that you entered is wrong").perform();
     }
@@ -112,10 +112,10 @@ public class A_SignUP_Page
     public void G_Check_that_ErrorAppear_whenInsert_EmailAlreadyExist()
     {
         clickRegisterButton() ;
-        retryType(FullName, "Nohair", 30);
-        retryType(Email, "gnohair@gmail.com", 30);
-        retryType(Password, "Qw222222", 30);
-        retryType(ConfirmPassword, "Qw222222", 30);
+        retryType(FullName, "Nohair", 40);
+        retryType(Email, "gnohair@gmail.com", 40);
+        retryType(Password, "Qw222222", 40);
+        retryType(ConfirmPassword, "Qw222222", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         By Error = By.xpath("//*[@id=\"heatmapArea\"]/div[17]");
         driver.element().verifyThat(Error).isVisible().perform();
@@ -125,8 +125,8 @@ public class A_SignUP_Page
     public void H_Check_that_ErrorAppear_whenPasswordANDConfirmPassword_areEmpty()
     {
         clickRegisterButton() ;
-        retryType(Password, " ", 30);
-        retryType(ConfirmPassword, " ", 30);
+        retryType(Password, " ", 40);
+        retryType(ConfirmPassword, " ", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(PasswordError).text().isEqualTo("Your password should be a minimum of 8 characters and a maximum of 20 characters. It should also contain the following: 1 Uppercase Letter, 1 Lowercase Letter, & 1 Number.").perform();
         driver.element().verifyThat(ConfirmPasswordError).text().isEqualTo("Confirm Password must be at least 8 characters").perform();
@@ -136,8 +136,8 @@ public class A_SignUP_Page
     public void I_Check_that_ErrorAppear_whenPasswordANDConfirmPassword_NotMatch()
     {
         clickRegisterButton() ;
-        retryType(Password, "Qw222222", 30);
-        retryType(ConfirmPassword, "As333333", 30);
+        retryType(Password, "Qw222222", 40);
+        retryType(ConfirmPassword, "As333333", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(ConfirmPasswordError).text().isEqualTo("The new password and confirm password do not match.").perform();
     }
@@ -146,8 +146,8 @@ public class A_SignUP_Page
     public void J_Check_that_ErrorAppear_whenPassword_less_than_8chars()   //password less than 8 chars
     {
         clickRegisterButton() ;
-        retryType(Password, "Qw22222", 30);
-        retryType(ConfirmPassword, "Qw22222", 30);
+        retryType(Password, "Qw22222", 40);
+        retryType(ConfirmPassword, "Qw22222", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(PasswordError).text().isEqualTo("Your password should be a minimum of 8 characters and a maximum of 20 characters. It should also contain the following: 1 Uppercase Letter, 1 Lowercase Letter, & 1 Number.").perform();
     }
@@ -156,8 +156,8 @@ public class A_SignUP_Page
     public void K_Check_that_ErrorAppear_whenPassword_Greater_than_20chars()   //password greater than 20 chars
     {
         clickRegisterButton() ;
-        retryType(Password, "Qw2222222222222222222", 30);
-        retryType(ConfirmPassword, "Qw2222222222222222222", 30);
+        retryType(Password, "Qw2222222222222222222", 40);
+        retryType(ConfirmPassword, "Qw2222222222222222222", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(PasswordError).text().isEqualTo("Your password should be a minimum of 8 characters and a maximum of 20 characters. It should also contain the following: 1 Uppercase Letter, 1 Lowercase Letter, & 1 Number.").perform();
     }
@@ -166,8 +166,8 @@ public class A_SignUP_Page
     public void L_Check_that_ErrorAppear_whenPassword_notContain_small_letter()   //password don't contain small letter
     {
         clickRegisterButton() ;
-        retryType(Password, "QQ222222", 30);
-        retryType(ConfirmPassword, "QQ222222", 30);
+        retryType(Password, "QQ222222", 40);
+        retryType(ConfirmPassword, "QQ222222", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(PasswordError).text().isEqualTo("Your password should be a minimum of 8 characters and a maximum of 20 characters. It should also contain the following: 1 Uppercase Letter, 1 Lowercase Letter, & 1 Number.").perform();
     }
@@ -176,8 +176,8 @@ public class A_SignUP_Page
     public void M_Check_that_ErrorAppear_whenPassword_notContain_Capital_letter()   //password don't contain Big letter
     {
         clickRegisterButton() ;
-        retryType(Password, "qq222222", 30);
-        retryType(ConfirmPassword, "qq222222", 30);
+        retryType(Password, "qq222222", 40);
+        retryType(ConfirmPassword, "qq222222", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(PasswordError).text().isEqualTo("Your password should be a minimum of 8 characters and a maximum of 20 characters. It should also contain the following: 1 Uppercase Letter, 1 Lowercase Letter, & 1 Number.").perform();
     }
@@ -186,8 +186,8 @@ public class A_SignUP_Page
     public void N_Check_that_ErrorAppear_whenPassword_notContain_Number()   //password don't contain Number
     {
         clickRegisterButton() ;
-        retryType(Password, "QQwwwwww", 30);
-        retryType(ConfirmPassword, "QQwwwwww", 30);
+        retryType(Password, "QQwwwwww", 40);
+        retryType(ConfirmPassword, "QQwwwwww", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         driver.element().verifyThat(PasswordError).text().isEqualTo("Your password should be a minimum of 8 characters and a maximum of 20 characters. It should also contain the following: 1 Uppercase Letter, 1 Lowercase Letter, & 1 Number.").perform();
     }
@@ -203,10 +203,10 @@ public class A_SignUP_Page
     public void O_Check_that_SignUp_workCorrectly()
     {
         clickRegisterButton() ;
-        retryType(FullName, "Nohair", 30);
-        retryType(Email, getRandomEmail(), 30);
-        retryType(Password, "Qw222222", 30);
-        retryType(ConfirmPassword, "Qw222222", 30);
+        retryType(FullName, "Nohair", 40);
+        retryType(Email, getRandomEmail(), 40);
+        retryType(Password, "Qw222222", 40);
+        retryType(ConfirmPassword, "Qw222222", 40);
         driver.element().keyPress(JoinNow_Button, ENTER);
         By Search_text = By.id("searchtext");
         driver.element().verifyThat(Search_text).isVisible() ;
